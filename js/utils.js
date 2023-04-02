b9fb84f00eebc17558e2befeff5f6768a8bb089b
ortozzz <evoak25@gmail.com>
2023-04-02 13:04:45 +0500

    t7p2

--- js/utils.js
+++ js/utils.js
@@ -1,22 +1,13 @@
-const getRandomInteger = function (min, max) {
-  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
-  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+function generateRandom(a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
   const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-const createRandomIdFromRangeGenerator = function (min, max) {
-  const previousValues = [];
-  return function () {
-    let currentValue = getRandomInteger(min, max);
 
-    if (previousValues.length >= max - min + 1) {
-      return null;
-    }
-    while (previousValues.includes(currentValue)) {
-      currentValue = getRandomInteger(min, max);
-    }
-    previousValues.push(currentValue);
-    return currentValue;
-  };
-};
-export { createRandomIdFromRangeGenerator, getRandomInteger };
+  return Math.floor(result);
+}
+
+function checkLength(verifiedString, maxLength) {
+  return (verifiedString.length >= maxLength);
+}
+
+export {generateRandom, checkLength};
